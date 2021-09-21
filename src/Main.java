19411cd99b7a30256f549e1bacc65fd37721ee22
PetrIvChe <cherniavskiipetr@gmail.com>
2021-09-21 23:08:50 +0300

    Two parametrs

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
-
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double bodyMassIndex = service.calculate(1.79);
+        double bodyMassIndex = service.calculate(1.72, 80);
         System.out.println("Ваш индекс массы тела " + bodyMassIndex);
-
     }
-    }
+}
 
